dcbecbb4d77f46d8ceac4f02bde247e9b83bb9a1
barth2501 <lancelot2501>
2020-02-18 16:08:01 +0100

    argparse added

--- main.py
+++ main.py
@@ -2,10 +2,18 @@ from command import Game
 from client import Player
 from h1 import *
 import time
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', '-n', help="Name of the player", type=str, required=True, default='JPP')
+parser.add_argument('--host', '-h', help="the host to use", type=str, required=True, default='127.0.0.1')
+parser.add_argument('--port', '-p', help="the port to use", type=str, required=True, default='5555')
+args = parser.parse_args()
 
 if __name__ == "__main__":
-    Game = Game('192.168.56.1')
-    Player = Player('barth', Game)
+    Game = Game(args.host, args.port)
+    Player = Player(args.name, Game)
     print(Game._type)
     while True:
         Game.update_map()
