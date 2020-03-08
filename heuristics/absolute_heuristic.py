5a2411d440f79e8b89de7acec476d671315fe75f
hinosxz <gauthier.henon@datadoghq.com>
2020-03-08 17:10:07 +0100

    Implement alpha beta Without splits

--- heuristics/absolute_heuristic.py
+++ heuristics/absolute_heuristic.py
@@ -1,15 +1,15 @@
+from numpy import ndarray
+
 from lib.positions import get_human_positions, get_opponent_positions, get_our_positions
-from models.game import Game
 
 from lib.util import manhattan_dist
 
 
-def absolute_heuristic(game: Game):
+def absolute_heuristic(state: ndarray, game_type: str):
     w_adv, w_hum = 1, 1
-    m = game.get_map()
-    houses_dict = get_human_positions(m)
-    opponent_dict = get_opponent_positions(m, game.type)
-    our_dict = get_our_positions(m, game.type)
+    houses_dict = get_human_positions(state)
+    opponent_dict = get_opponent_positions(state, game_type)
+    our_dict = get_our_positions(state, game_type)
     score = 0
 
     # calculate our score regarding the current state of the map
@@ -20,11 +20,15 @@ def absolute_heuristic(game: Game):
         for oppo_position in opponent_dict.keys():
             nb_oppo = opponent_dict[oppo_position]
             distance_us_oppo = manhattan_dist(us_position, oppo_position)
-            value += w_adv / distance_us_oppo * (nb_us / nb_oppo - 3 / 2)
+            # FIXME find better alternative to avoid division by zero
+            if distance_us_oppo and nb_oppo:
+                value += w_adv / distance_us_oppo * (nb_us / nb_oppo - 3 / 2)
         for human_position in houses_dict.keys():
             nb_human = houses_dict[human_position]
             distance_us_human = manhattan_dist(us_position, human_position)
-            value += w_hum / distance_us_human * (nb_us / nb_human - 1)
+            # FIXME find better alternative to avoid division by zero
+            if distance_us_human and nb_human:
+                value += w_hum / distance_us_human * (nb_us / nb_human - 1)
         value_list.append(value)
         all_neg = True
         for val in value_list:
@@ -43,10 +47,14 @@ def absolute_heuristic(game: Game):
         for us_position in our_dict.keys():
             nb_us = our_dict[us_position]
             distance_oppo_us = manhattan_dist(oppo_position, us_position)
-            value += w_adv / distance_oppo_us * (nb_oppo / nb_us - 3 / 2)
+            # FIXME find better alternative to avoid division by zero
+            if distance_oppo_us and nb_us:
+                value += w_adv / distance_oppo_us * (nb_oppo / nb_us - 3 / 2)
         for human_position in houses_dict.keys():
             nb_human = houses_dict[human_position]
             distance_oppo_human = manhattan_dist(oppo_position, human_position)
-            value += w_hum / distance_oppo_human * (nb_oppo / nb_human - 1)
+            # FIXME find better alternative to avoid division by zero
+            if distance_oppo_human and nb_human:
+                value += w_hum / distance_oppo_human * (nb_oppo / nb_human - 1)
         score -= value
     return score
---
+++ lib/alpha-beta.py
@@ -0,0 +1,126 @@
+from itertools import product
+from numpy import inf as infinity, ndarray, array, copy
+from typing import Iterable, Tuple, List
+
+from heuristics.absolute_heuristic import absolute_heuristic
+from lib.constants import TYPE_TO_POSITION_INDEX, TYPE_TO_OPPONENT_POSITION_INDEX
+from lib.positions import get_positions
+
+
+OPPONENTS = {"vampire": "wolf", "wolf": "vampire"}
+
+
+def game_is_over(state: ndarray):
+    vampires = get_positions(state, 1).keys()
+    werewolves = get_positions(state, 2).keys()
+    return len(vampires) == 0 or len(werewolves) == 0
+
+
+def cutoff_test(state: ndarray, depth: int, max_depth: int):
+    return depth > max_depth or game_is_over(state)
+
+
+def evaluate(state: ndarray, game_type: str):
+    return absolute_heuristic(state, game_type)
+
+
+def get_neighbors(cell, shape):
+    p, q = shape[0], shape[1]
+    i, j = cell
+    return [
+        (x2, y2)
+        for x2 in range(i - 1, i + 2)
+        for y2 in range(j - 1, j + 2)
+        if (
+            -1 < i < p
+            and -1 < j < q
+            and (i != x2 or j != y2)
+            and (0 <= x2 < p)
+            and (0 <= y2 < q)
+        )
+    ] + [cell]
+
+
+def get_moves(
+    group_position: Tuple[int, int], size: int, neighbors: List[Tuple[int, int]]
+):
+    return [(*group_position, size, *to) for to in neighbors]
+
+
+def get_successors(state: ndarray, species: int):
+    groups = get_positions(state, species)
+
+    possible_moves_per_group = [
+        get_moves(group_position, size, get_neighbors(group_position, state.shape))
+        for group_position, size in groups.items()
+    ]
+    possible_moves: Iterable[Tuple[Tuple[int, int, int, int, int], ...]] = product(
+        *possible_moves_per_group
+    )
+
+    successors: List[ndarray] = []
+    for moves in possible_moves:
+        successor = copy(state)
+        for x_origin, y_origin, size, x_target, y_target in moves:
+            successor[x_origin, y_origin, species] -= size
+            successor[x_target, y_target, species] += size
+        successors.append(successor)
+
+    return successors
+
+
+def alphabeta_search(species_played: str, state: ndarray, d=4):
+    """
+
+    :param species_played: Current game
+    :param state: State of the current game
+    :param d: Maximum depth
+    :return: An action
+    """
+
+    def max_value(s: ndarray, alpha: int, beta: int, depth: int):
+        if cutoff_test(s, depth, d):
+            return evaluate(s, species_played), s
+        v = -infinity
+        next_state = s
+        for successor_state in get_successors(
+            state, TYPE_TO_POSITION_INDEX[species_played]
+        ):
+            next_min = min_value(successor_state, alpha, beta, depth + 1)[0]
+            if next_min > v:
+                v = next_min
+                next_state = successor_state
+            if v >= beta:
+                return v, next_state
+            alpha = max(alpha, v)
+        return v, next_state
+
+    def min_value(s: ndarray, alpha: int, beta: int, depth: int):
+        if cutoff_test(s, depth, d):
+            return evaluate(s, OPPONENTS[species_played]), s
+        v = infinity
+        next_state = s
+        for successor_state in get_successors(
+            state, TYPE_TO_OPPONENT_POSITION_INDEX[species_played]
+        ):
+            next_max = max_value(successor_state, alpha, beta, depth + 1)[0]
+            if next_max < v:
+                v = next_max
+                next_state = successor_state
+            if v <= alpha:
+                return v, next_state
+            beta = min(beta, v)
+        return v, next_state
+
+    return min_value(state, -infinity, infinity, 0)
+
+
+# Example state to test
+# example = array( [
+#         [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 2, 0], [0, 0, 0], [0, 0, 0]],
+#         [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],
+#         [[0, 0, 0], [0, 0, 0], [1, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]],
+#         [[0, 0, 0], [0, 3, 0], [0, 0, 3], [0, 0, 0], [0, 0, 0], [0, 0, 0]],
+#         [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [1, 0, 0], [0, 0, 0]],
+#     ])
+# print(alphabeta_search("vampire", example))
