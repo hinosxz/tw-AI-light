87c2b4bbcdb3f22d9b089a2d0d12419584d7672a
Astings <alexandre.laine@student.ecp.fr>
2020-03-13 14:42:33 +0100

    Compute groups improved

--- lib/compute_groups.py
+++ lib/compute_groups.py
@@ -1,44 +1,86 @@
-ennemy_positions = {"a": 2, "b": 2, "c": 2}
+from math import ceil
+import itertools
+from heuristics.heuristic1 import distance_nb_coups
+from operator import itemgetter
+from copy import copy
 
-human_positions = {"a": 4}
+ennemy_positions = {(2, 3): 2, (4, 3): 2, (5, 7): 2}
 
-our_positions = {"a": 10}
+human_positions = {(10, 3): 4}
+
+our_positions = {(2, 8): 11}
 
 
 def compute_groups(our_positions: dict, ennemy_positions: dict, human_positions: dict):
-
     our_size = 0
 
-    for value in our_positions.values():
-        our_size = value
+    for key, value in our_positions.items():
+        our_size = [key, value]
 
     sizes = []
     possibilities = [[our_size]]
 
-    for value in ennemy_positions.values():
-        sizes.append(1.5 * value)
+    for key, value in ennemy_positions.items():
+        sizes.append([key, ceil(1.5 * value)])
 
-    for value in human_positions.values():
-        sizes.append(value)
+    for key, value in human_positions.items():
+        sizes.append([key, value])
 
-    sizes = sorted(sizes)
-    print(sizes)
+    sizes = sorted(sizes, key=itemgetter(1))
     for i in range(len(sizes)):
-        print("Indices for " + str(i))
         j = i
-        buffer_size = our_size
+        buffer_size = copy(our_size)
         buffer_split = []
+        # TODO Limit minimal split size in a smarter way
+        while j < len(sizes) and (buffer_size[1] - sizes[j][1]) >= sizes[j][1]:
 
-        while j < len(sizes) and (buffer_size - sizes[j]) >= sizes[0]:
-            print("Indices while " + str(j))
-            buffer_size = buffer_size - sizes[j]
-            buffer_split.append(sizes[j])
-            print(
-                "Buffer size " + str(buffer_size) + " Buffer split" + str(buffer_split)
-            )
-            possibility = [buffer_size] + buffer_split
-            print(possibility)
+            buffer_size[1] = buffer_size[1] - sizes[j][1]
+            buffer_split.append([find_closest(our_size[0], sizes[j][0]), sizes[j][1]])
+            possibility = [copy(buffer_size)] + buffer_split
             possibilities += [possibility]
             j += 1
 
-    return set(tuple(i) for i in possibilities)
+    output = []
+    for possibility in possibilities:
+
+        j = len(sizes) - 1
+        while possibility[0][1] < sizes[j][1]:
+            j -= 1
+        possibility[0][0] = find_closest(possibility[0][0], sizes[j][0])
+
+        if possibility not in output:
+            output.append(possibility)
+    return output
+
+
+def find_closest(our_position, target_position):
+    if our_position[0] == target_position[0] and our_position[1] > target_position[1]:
+        x0, y0 = our_position[0], our_position[1] - 1
+
+    elif our_position[0] == target_position[0] and our_position[1] < target_position[1]:
+        x0, y0 = our_position[0], our_position[1] + 1
+
+    elif our_position[1] == target_position[1] and our_position[0] > target_position[0]:
+        x0, y0 = our_position[0] - 1, our_position[1]
+
+    elif our_position[1] == target_position[1] and our_position[0] < target_position[0]:
+        x0, y0 = our_position[0] + 1, our_position[1]
+
+    elif our_position[0] > target_position[0] and our_position[1] > target_position[1]:
+        x0, y0 = our_position[0] - 1, our_position[1] - 1
+
+    elif our_position[0] > target_position[0] and our_position[1] < target_position[1]:
+        x0, y0 = our_position[0] - 1, our_position[1] + 1
+
+    elif our_position[0] < target_position[0] and our_position[1] > target_position[1]:
+        x0, y0 = our_position[0] + 1, our_position[1] - 1
+
+    elif our_position[0] < target_position[0] and our_position[1] < target_position[1]:
+        x0, y0 = our_position[0] + 1, our_position[1] + 1
+
+    return tuple((x0, y0))
+
+
+output = compute_groups(our_positions, ennemy_positions, human_positions)
+print("Returned : " + str(output))
+print("Length : " + str(len(output)))
