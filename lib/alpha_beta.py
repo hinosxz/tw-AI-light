ae3bb05707ebe998b7ef1f3ee30aec970209c743
Astings <alexandre.laine@student.ecp.fr>
2020-03-18 14:16:08 +0100

    Added limit of split

--- lib/alpha_beta.py
+++ lib/alpha_beta.py
@@ -73,12 +73,12 @@ def get_moves(
     return [(*group_position, size, *to) for to in neighbors]
 
 
-def get_successors(state: ndarray, species: str):
+def get_successors(state: ndarray, species: str, groups_limit: int):
     our_groups = get_our_positions(state, species)
     their_groups = get_opponent_positions(state, species)
     human_groups = get_human_positions(state)
-
-    possible_moves = compute_groups(our_groups, their_groups, human_groups, 1000)
+    limit = groups_limit - len(our_groups)
+    possible_moves = compute_groups(our_groups, their_groups, human_groups, limit)
 
     successors: List[ndarray] = []
     for moves in possible_moves:
@@ -130,7 +130,7 @@ def check_conflict(current_cell: ndarray, player_index: int):
 
 
 def alphabeta_search(
-    species_played: str, state: ndarray, d=4, heuristic_played: str = "heuristic2"
+    species_played: str, state: ndarray, d=4, heuristic_played: str = "heuristic2", groups_limit=4
 ):
     """
 
@@ -138,6 +138,7 @@ def alphabeta_search(
     :param species_played: Current game
     :param state: State of the current game
     :param d: Maximum depth
+    :param groups_limit: Maximum number of groups allowed
     :return: An action
     """
 
@@ -162,7 +163,7 @@ def alphabeta_search(
         next_state = s
         next_moves = moves
         successor_states, successor_move_options = get_successors(
-            s, TYPE_TO_POSITION_INDEX[species_played]
+            s, TYPE_TO_POSITION_INDEX[species_played], groups_limit
         )
         for k in range(len(successor_move_options)):
             successor_state = successor_states[k]
@@ -218,7 +219,7 @@ def alphabeta_search(
         next_state = s
         next_moves = moves
         successor_states, successor_move_options = get_successors(
-            s, TYPE_TO_OPPONENT_POSITION_INDEX[species_played]
+            s, TYPE_TO_OPPONENT_POSITION_INDEX[species_played], groups_limit
         )
         for k in range(len(successor_move_options)):
             successor_state = successor_states[k]
