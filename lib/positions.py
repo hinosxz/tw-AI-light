d81134c69d091f2e963285fb293546b1e0d3d8ec
hinosxz <gauthier.henon@datadoghq.com>
2020-03-31 16:15:04 +0200

    Rename map into state

--- lib/positions.py
+++ lib/positions.py
@@ -5,22 +5,22 @@ from numpy import ndarray
 from lib.constants import TYPE_TO_OPPONENT_POSITION_INDEX, TYPE_TO_POSITION_INDEX
 
 
-def get_positions(map: ndarray, index: int):
+def get_positions(state: ndarray, index: int):
     positions: Dict[Tuple[int, int], int] = {}
-    for i, row in enumerate(map):
+    for i, row in enumerate(state):
         for j, cell in enumerate(row):
             if cell[index] != 0:
                 positions[(i, j)] = cell[index]
     return positions
 
 
-def get_human_positions(map: ndarray):
-    return get_positions(map, 0)
+def get_human_positions(state: ndarray):
+    return get_positions(state, 0)
 
 
-def get_opponent_positions(map: ndarray, type: str):
-    return get_positions(map, TYPE_TO_OPPONENT_POSITION_INDEX[type])
+def get_opponent_positions(state: ndarray, species_played: str):
+    return get_positions(state, TYPE_TO_OPPONENT_POSITION_INDEX[species_played])
 
 
-def get_our_positions(map: ndarray, type: str):
-    return get_positions(map, TYPE_TO_POSITION_INDEX[type])
+def get_our_positions(state: ndarray, species_played: str):
+    return get_positions(state, TYPE_TO_POSITION_INDEX[species_played])
